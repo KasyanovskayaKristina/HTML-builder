aed9ca9f558b35e7cd9b366686b387ea8ce95098
Kasyanovskaya Kristina <kristinochkakv@gmail.com>
2022-11-10 14:44:22 +0300

    init: change folders

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-let stream = new fs.ReadStream("text.txt", "utf8");
+let stream = new fs.ReadStream("01-read-file/text.txt", "utf8");
 stream.on("readable", function () {
   let data = stream.read();
   if (data != null) console.log(data);
